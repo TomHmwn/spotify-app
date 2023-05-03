4383c0aaff186740916ca0131610e1c45256aec6
Thomas Himawan <thimawan@student.unimelb.edu.au>
2023-05-03 10:45:15 +1000

    remove git in client

---
+++ client/src/styles/fonts.js
@@ -0,0 +1,23 @@
+import { css } from 'styled-components/macro';
+
+const fonts = css`
+  @font-face {
+    font-family: sans-serif;
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: sans-serif;
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: sans-serif;
+    font-weight: 900;
+    font-style: normal;
+  }
+`;
+
+export default fonts;
